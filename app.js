ae7b5f057a1cf553a4160124bc08553259bed549
YonghoLee11 <kamsyyongho@naver.com>
2022-01-05 07:19:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 import React, {  useState } from 'react'
 import Items from './components/Items'
 // import MaterialTable from 'material-table'
-import {Menu} from '@material-ui/core'
+//import {Menu} from '@material-ui/core'
+import Menu from './components/Menu'
+
 
 function App(props) {
 
@@ -16,7 +18,7 @@ function App(props) {
   const call_api = () =>{
     console.log(search_text.value)
     setItems([])
-    axios.get('http://10.144.101.51:5000/find_themes_items/' + search_text.value)
+    axios.get('http://192.168.0.175:5000/find_themes_items/' + search_text.value)
     .then((Response)=>{
       console.log(Response.data.items)
       setItems(Response.data.items)
@@ -24,50 +26,16 @@ function App(props) {
     .catch((Error)=>{console.log(Error)})
   }
 
-  // const columns = [
-  //   {title : "amount" , field : "amount"},
-  //   {title : "ask_buy" , field : "ask_buy"},
-  //   {title : "ask_sell" , field : "ask_sell"},
-  //   {title : "current_price" , field : "current_price"},
-  //   {title : "event_name" , field : "event_name"},
-  //   {title : "event_no" , field : "event_no"},
-  //   {title : "fluctuation_rate" , field : "fluctuation_rate"},
-  //   {title : "theme_name" , field : "theme_name"}, 
-  //   {title : "transfer_reason" , field : "transfer_reason"}, 
-  // ]
-
     return(
       <div className="App">
-        <Menu
-          id="menu-appbar"
-          anchorEl={null}
-          getContentAnchorEl={null}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-          transformOrigin={{ vertical: "top", horizontal: "center" }}
-          open={open}
-          onClose={null}
-          className={'null'}        
-        />
+        
         <input type="text" value={search_text.value} onChange={(e)=>handleChange(e)}></input>
         <button onClick={call_api}>검색</button> <br></br>
 
+        <Menu/>
         <Items items_arr={items_arr} on_item_change={setItems}></Items>
-
-        {/* {items_arr.map((item , index) => <span key={index}>{item.event_name}</span>)} */}
       </div>
     )  
-
-    // return(
-    //   <div className='App'>
-    //     <input type="text" value={search_text.value} onChange={(e)=>handleChange(e)}></input>
-    //     <button onClick={call_api}>검색</button> <br></br>         
-    //     <MaterialTable
-    //       title="stock"
-    //       data={items_arr}
-    //       columns={columns}
-    //     />
-    //   </div>
-    // )
   }
 
-export default App;
+export default App
